1af2b17bea0699bbe3b3936f8aceaa3b04f21e1c
Mate Gonczi <gamte19@gmail.com>
2016-07-06 17:38:42 +0200

    browse the web function

--- smart_car_radio.py
+++ smart_car_radio.py
@@ -4,8 +4,9 @@ from car_radio import CarRadio
 
 class SmartCarRadio(PhoneCall, CarRadio):
 
-    def __init__(self, *args):
+    def __init__(self, data_limit, *args):
         super().__init__(*args)
+        self.data_limit = data_limit
 
     def normalize_phone_number(self):
         self.phone_number = self.phone_number.replace(" ", "").replace("-", "")
@@ -14,3 +15,8 @@ class SmartCarRadio(PhoneCall, CarRadio):
     def make_normalized_number_calling(self, number):
         normalized_call = self.normalize_phone_number()
         print("Im calling " + number + " and my number is " + self.phone_number)
+
+    def browse_the_web(self):
+        print("Facebok")
+        print("your data limit " + str(self.data_limit) + "MB")
+        self.data_limit -= 1
